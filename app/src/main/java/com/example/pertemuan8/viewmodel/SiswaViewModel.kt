586b3a5cd7c28e118a89671f6a01cee1465b531f
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-20 09:23:19 +0700

    membuat import

--- app/src/main/java/com/example/pertemuan8/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/pertemuan8/viewmodel/SiswaViewModel.kt
@@ -1,5 +1,13 @@
 package com.example.pertemuan8.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+
 class SiswaViewModel : ViewModel() {
     private val _statusUI = MutableStateFlow(Siswa())
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()

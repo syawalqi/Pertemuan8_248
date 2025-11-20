15cfaa3fac3c0c0b15e7cc67a3eb368925a8ab9f
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-20 09:19:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan8/model/DataJK.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan8.model
+import com.example.pertemuan8.R
+
+object DataJK {
+    val JenisJK= listOf(
+        R.string.laki,
+        R.string.wanita,
+    )
+}
---
+++ app/src/main/java/com/example/pertemuan8/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan8.viewmodel
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa()
+
+}

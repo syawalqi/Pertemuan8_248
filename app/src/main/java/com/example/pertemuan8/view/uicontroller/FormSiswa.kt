340bd08b99fc9d5c66ca3045e199cbfb8dabcabf
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-20 14:33:52 +0700

    minor fixes

--- app/src/main/java/com/example/pertemuan8/view/uicontroller/FormSiswa.kt
+++ app/src/main/java/com/example/pertemuan8/view/uicontroller/FormSiswa.kt
@@ -8,6 +8,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.pertemuan8.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,7 +44,7 @@ fun FormSiswa(
             TopAppBar(
                 title = { Text(text = stringResource(id = R.string.app_name), color = Color.White) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = colorResource(id = R.color.purple)
+                    containerColor = colorResource(id = R.color.purple_500)
                 )
             )
         }
@@ -54,10 +71,9 @@ fun FormSiswa(
                 modifier = Modifier
                     .padding(all = 12.dp)
                     .width(250.dp),
-                thickness = dimensionResource(1.dp),
+                thickness = dimensionResource(id = R.dimen.thickness_divider),
                 color = Color.Blue
             )
-
             Row {
                 pilihanJK.forEach { item ->
                     Row(
@@ -86,7 +102,7 @@ fun FormSiswa(
                 modifier = Modifier
                     .padding(all = 5.dp)
                     .width(250.dp),
-                thickness = dimensionResource(1.dp),
+                thickness = dimensionResource(id = R.dimen.thickness_divider),
                 color = Color.Blue
             )
 

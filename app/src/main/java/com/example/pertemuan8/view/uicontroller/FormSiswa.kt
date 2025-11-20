d585b4fc3133e5adf8c08ddde40ba380bf42d9f1
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-20 09:40:37 +0700

    membuat fun Form siswa/ Scaffold/ topappbar/ colors

--- app/src/main/java/com/example/pertemuan8/view/uicontroller/FormSiswa.kt
+++ app/src/main/java/com/example/pertemuan8/view/uicontroller/FormSiswa.kt
@@ -1,5 +1,14 @@
 package com.example.pertemuan8.view.uicontroller
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormSiswa(
@@ -14,7 +23,9 @@ fun FormSiswa(
 
 
     Scaffold(modifier = Modifier,
-        {TopAppBar(title = { Text(Text(StringResource(id = "Form Pendaftaran")
+        {TopAppBar(title = { Text(Text(StringResource(id = "Form Pendaftaran"), color = Color.White,
+            colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.purple)
+            )
 
 
 }

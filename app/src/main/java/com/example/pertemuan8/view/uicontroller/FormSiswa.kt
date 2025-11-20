9c1203777eaa1114768516b2e9d441b041f0474f
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-20 09:34:19 +0700

    membuat fun Form siswa

--- app/src/main/java/com/example/pertemuan8/view/uicontroller/FormSiswa.kt
+++ app/src/main/java/com/example/pertemuan8/view/uicontroller/FormSiswa.kt
@@ -1,2 +1,14 @@
 package com.example.pertemuan8.view.uicontroller
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa(
+    pilihanJK : List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+){
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("")}
+    var txtGender by remember { mutableStateOf("")}
+    val listData: MutableList<String> =mutableListOf(txtNama, txtGender, txtAlamat)}
+}

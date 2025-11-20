e4d866da1d15d5493287e3822a0ebbe865522565
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-20 09:22:10 +0700

    membuat fun setsiswa/ statusUI

--- app/src/main/java/com/example/pertemuan8/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/pertemuan8/viewmodel/SiswaViewModel.kt
@@ -5,6 +5,11 @@ class SiswaViewModel : ViewModel() {
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
 
     fun setSiswa(ls:MutableList<String>){
+        _statusUI.update { statusSaatini ->
+            statusSaatini.copy(nama = ls[0], gender = ls[1], alamat = ls[2])
+        }
+    }
+
 
     })
 

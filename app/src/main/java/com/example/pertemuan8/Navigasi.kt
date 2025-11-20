35a9c6618cee01a14c28a09f3d0576df22ba4f4f
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-20 14:58:23 +0700

    membuat file Navigasi.kt konfigurasi

--- app/src/main/java/com/example/pertemuan8/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan8/Navigasi.kt
@@ -1,5 +1,20 @@
 package com.example.pertemuan8
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.collectAsState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.platform.LocalContext
+import com.example.pertemuan8.view.uicontroller.FormSiswa
+import com.example.pertemuan8.view.uicontroller.TampilSiswa
+import com.example.pertemuan8.viewmodel.SiswaViewModel
+import com.example.pertemuan8.model.DataJK
+
 enum class Navigasi {
     Formulir,
     Detail
@@ -8,33 +23,34 @@ enum class Navigasi {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaApp(
-    // edit 1: parameter viewModel
     viewModel: SiswaViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
-    // edit 2: Tambahkan variabel uiState
     val uiState by viewModel.statusUI.collectAsState()
+
     NavHost(
         navController = navController,
-        startDestination = Navigasi.Formulir.name,
-        modifier = Modifier.padding(paddingValues = isiRuang)
+        startDestination = Navigasi.Formulir.name
     ) {
-        composable(route = Navigasi.Formulir.name) {
-            // edit 3 : Tambahkan variable konteks
+
+        composable(Navigasi.Formulir.name) {
+
             val konteks = LocalContext.current
+
             FormSiswa(
-                // edit 4 : parameter pilihanJK dan onsubmitButtonClicked
-                pilihanJK = JenisJK.map { id -> konteks.resources.getString(id) },
+                pilihanJK = DataJK.JenisJK.map { id ->
+                    konteks.resources.getString(id)
+                },
                 onSubmitButtonClicked = {
                     viewModel.setSiswa(it)
-                    navController.navigate(route = Navigasi.Detail.name)
+                    navController.navigate(Navigasi.Detail.name)
                 }
             )
         }
-        composable(route = Navigasi.Detail.name) {
+
+        composable(Navigasi.Detail.name) {
             TampilSiswa(
-                // edit 5 : parameter statusUISiswa
-                statusUISiswa = uiState.value,
+                statusUISiswa = uiState,
                 onBackButtonClicked = { cancelAndBackToFormulir(navController) }
             )
         }
@@ -44,5 +60,8 @@ fun SiswaApp(
 private fun cancelAndBackToFormulir(
     navHostController: NavHostController
 ) {
-    navController.popBackStack(route = Navigasi.Formulir.name, inclusive = false)
+    navHostController.popBackStack(
+        Navigasi.Formulir.name,
+        false
+    )
 }

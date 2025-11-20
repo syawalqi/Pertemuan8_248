fcbe0e4a8f4aa66c26c849e7a28f190a6c0c00ff
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-20 14:22:24 +0700

    membuat fun Form siswa/ scaffold

--- app/src/main/java/com/example/pertemuan8/view/uicontroller/FormSiswa.kt
+++ app/src/main/java/com/example/pertemuan8/view/uicontroller/FormSiswa.kt
@@ -20,3 +20,15 @@ fun FormSiswa(
     var txtAlamat by remember { mutableStateOf("") }
     var txtGender by remember { mutableStateOf("") }
     val listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
+
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.purple)
+                )
+            )
+        }
+    )
